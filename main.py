e037e2b16c4a96ecdcf173de0ac335039fb77e1b
Nick Pisarski <nick.pisarski@gmail.com>
2019-10-02 10:46:48 -0500

    created an empty project template

---
+++ main.py
@@ -0,0 +1,12 @@
+#!v-env/bin/python
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s: %(name)s - %(message)s",
+    datefmt="%m-%d-%Y %H:%M:%S",
+)
+
+
+if __name__ == "__main__":
+    pass
