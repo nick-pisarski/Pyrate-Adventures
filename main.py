e3b40c53016cdc6a9a6a4c876e9992ab63480f76
Nick Pisarski <nick.pisarski@gmail.com>
2019-11-21 11:33:12 -0600

    added logger creation and utilities

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 #!v-env/bin/python
 import logging
+from os import path
 
 logging.basicConfig(
     level=logging.INFO,
-    format="%(asctime)s %(levelname)s: %(name)s - %(message)s",
-    datefmt="%m-%d-%Y %H:%M:%S",
+    format="(%(asctime)s) %(name)s:  %(message)s",
+    datefmt="%m.%d.%y@%H:%M:%S",
 )
 
+log = logging.getLogger(path.basename(__file__))
+def main():
+    log.info('It works')
 
 if __name__ == "__main__":
-    pass
+    main()
