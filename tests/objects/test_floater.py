8c1be432335c54c38860a4f01660dc4a3ea97d34
Nick Pisarki <nick.pisarski@gmail.com>
2021-12-30 23:06:02 -0600

    added test files for entities

---
+++ tests/objects/test_floater.py
@@ -0,0 +1,15 @@
+from lib.objects import Floater
+
+class TestFloater:
+
+    def test_instantiation(self):
+        pass
+
+    def test_move(self):
+        pass
+
+    def test_reset(self):
+        pass
+
+    def test_is_offscreen(self):
+        pass
---
+++ tests/objects/test_player.py
@@ -0,0 +1,9 @@
+from lib.objects import Player
+
+
+class TestPlayer:
+    def test_instatiation(self):
+        pass
+
+    def test_move(self):
+        pass
